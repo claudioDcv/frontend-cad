a5366982ee2dec330866d39e90c2d3f15d60dcd0
Carolina Zurita <czurita@dataera.cl>
2025-04-25 23:52:41 -0400

    JWT validation

--- src/clients/fetchSecureData.ts
+++ src/clients/fetchSecureData.ts
@@ -1,18 +1,19 @@
 import { API_BASE } from '../conf/http';
-import { getToken } from '../hooks/useJWTNotification';
 
-export const fetchSecureData = async () => {
+export const fetchSecureData = async (jwt: string) => {
     const url = `${API_BASE}/api/v1/secure/data`;
     const response = await fetch(url, {
         method: 'GET',
         headers: {
-            'Authorization': `${getToken()}`,
+            'Authorization': `${jwt}`, // 👉 ahora usamos el parámetro
             'Content-Type': 'application/json'
         },
         credentials: 'include'
     });
+
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
+
     return response.json();
 };
